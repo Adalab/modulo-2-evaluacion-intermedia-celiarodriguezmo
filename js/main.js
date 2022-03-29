2d88f52110d80a101cd70bf8b52677f157141c90
celiarodriguezmo <celiro02@gmail.com>
2022-03-29 11:01:55 +0200

    add functions at javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,65 @@
 'use strict';
+const btnPlay = document.querySelector('.js_play');
+const options= document.querySelector('.js_select');
+const userBet= document.querySelector('.js_bet');
+const paintResult =document.querySelector('.js_divresult');
+const message = document.querySelector('.js_message');
+const balance = document.querySelector('.js_balance');
+
+const userChoice = options.value;
+
+//Función para generar un número aleatorio entre 1 y 6
+
+
+
+function getRandomNumber(max) { 
+    
+    return Math.ceil(Math.random() * max); 
+   
+  }
+
+
+function game(){
+
+    const numAleat = getRandomNumber (numAleat)
+  
+    
+}
+
+   
+function getChoiceUser (){
+    const userChoice= options.value;
+    paintResult.innerHTML= userChoice;
+
+}
+
+function getBetUser (){
+    
+    const howMuchMoneyBet = userBet.value;
+
+    if (numAleat === userChoice){
+
+    balance.innerHTML= howMuchMoneyBet*2;
+    message.innerHTML= "¡Has ganado!";}
+
+    else {
+    balance.innerHTML= howMuchMoneyBet*0;
+    message.innerHTML= "¡Has perdido!";}
+    
+}
+
+//Empleamos una funcion manejadora para manejar las funciones
+
+function handleClickPlay(event) {
+    event.preventDefault();
+  
+    getRandomNumber();
+  
+   getChoiceUser();
+
+
+}
+
+
+//escuchamos el evento cuando la usuaria clica en Play
+  btnPlay.addEventListener('click', handleClickPlay);
